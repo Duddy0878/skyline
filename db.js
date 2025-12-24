a70d044b46b83c531539042868443c3f3cdc02aa
Duddy0878 <duddystudio@gmail.com>
2025-12-24 16:03:28 -0500

    a

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host:  '127.0.0.1',
-  port: 3306,
-  user:  'root',
-  password:  'Duddy0878',
-  database:  'small material',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
