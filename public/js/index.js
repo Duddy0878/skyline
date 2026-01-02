4b5bc8037fd25a76a5369408b66235ebb2c17faa
Duddy0878 <duddystudio@gmail.com>
2026-01-02 10:18:36 -0500

    care

--- public/js/index.js
+++ public/js/index.js
@@ -23,7 +23,7 @@ import {fetchApi, fetchApiWithId , postApi} from './api.js'
                 let categoryHeader = document.createElement('div')
                 categoryHeader.className = 'categoryHeader'
                 let category = await fetchApiWithId('/category',item.cate_id)
-                categoryHeader.innerHTML = `<h2> ${category.name} </h2>`
+                categoryHeader.innerHTML = `<h2> ${upperCaseFirstLetter(category.name)} </h2>`
                 itemsDiv.appendChild(categoryHeader)
             }
             let category = await fetchApiWithId('/category',item.cate_id)
@@ -420,6 +420,10 @@ import {fetchApi, fetchApiWithId , postApi} from './api.js'
                 }
                 // Add inventory logic here
             });
+        }
+
+        function upperCaseFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
         }
 
         
