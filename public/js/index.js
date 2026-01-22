521062648373cf317620517ca71466d1f255a27f
Duddy0878 <duddystudio@gmail.com>
2026-01-22 14:01:29 -0500

    ccc

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,10 @@
 const target = document.querySelector('.select > div');
-if (target.classList.contains('rails')) {
-    target.addEventListener('click', (e) => {
-        e.preventDefault(); // Prevent default link behavior
-        window.location.href = "./html/order.html?type=rails";
-    });
-} else if (target.classList.contains('ropes')) {
-    target.addEventListener('click', (e) => {
-        e.preventDefault(); // Prevent default link behavior
-        window.location.href = "./html/order.html?type=ropes";
-    });
-}
+target.addEventListener('click', () => {
+
+    if(target.classList.contains('rails')){
+        window.location.href = '/html/order.html?type=rails'
+    }
+    else if(target.classList.contains('ropes')){
+        window.location.href = '/html/order.html?type=ropes'
+    }
+})
