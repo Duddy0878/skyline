32d167296d5b8ec94c99402811c7547292b885e1
Duddy0878 <duddystudio@gmail.com>
2026-01-22 13:58:33 -0500

    ere4e

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,12 @@
 const target = document.querySelector('.select > div');
-if (target) {
+if (target.classList.contains('rails')) {
     target.addEventListener('click', (e) => {
         e.preventDefault(); // Prevent default link behavior
         window.location.href = "./html/order.html?type=rails";
+    });
+} else if (target.classList.contains('ropes')) {
+    target.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent default link behavior
+        window.location.href = "./html/order.html?type=ropes";
     });
 }
