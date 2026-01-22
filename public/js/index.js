0cf60d8186b6fbc5a7e9f7b95b713180812a1ebc
Duddy0878 <duddystudio@gmail.com>
2026-01-22 14:02:43 -0500

    vv

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
-const target = document.querySelector('.select > div');
-target.addEventListener('click', () => {
+const target = document.querySelector('.select');
+target.addEventListener('click', (e) => {
 
-    if(target.classList.contains('rails')){
+    if(e.target.classList.contains('rails')){
         window.location.href = '/html/order.html?type=rails'
     }
-    else if(target.classList.contains('ropes')){
+    else if(e.target.classList.contains('ropes')){
         window.location.href = '/html/order.html?type=ropes'
     }
 })
